2487d4845cfed1e22942649a12b7011320e4d11a
Nicoo <acenico@hotmail.fr>
2021-08-31 11:34:32 +0200

    Add goldminig

---
+++ resources/[FAROESTE]/[JOBS]/frp_gold/config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+Config.Recolte =  {
+    vector3(-1132.923,-4.139,40.998)
+
+}
+
+Config.Traitement = {
+    vector3(-1123.857,-3.452,41.859)
+
+}
+
+
+Config.Revente = {
+    vector3(3016.469,1349.540,42.771)
+}
+
